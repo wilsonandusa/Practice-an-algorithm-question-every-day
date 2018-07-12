4e6ff75773c5e9dcb39a8c6b89bcbd1684952feb
wilsonandiphone@icloud.com <wilsonandiphone@icloud.com>
2018-07-11 22:30:45 -0700

    add

---
+++ chapter_Arrays&Strings/leetcode/wordLadder.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+       			if(!wordList.contains(endWord)) return 0;
+		Queue<String> queue = new LinkedList<String>();
+		queue.add(beginWord);
+		int level = 0;
+		while(!queue.isEmpty()) {
+			level++;
+			int size = queue.size();
+			for(int i = 0; i < size; i++) {
+				String cur = queue.poll();
+				if(cur.equals(endWord)) return level;
+				for(Iterator<String> it =  wordList.iterator(); it.hasNext(); ) {
+					String str = it.next();
+					if(this.check(str, cur)) {
+						it.remove();
+						queue.add(str);
+					}
+				}
+			}
+		}
+		return 0;
+    }
+    
+    public boolean check(String str1, String str2) {
+		char[] arr1 = str1.toCharArray();
+		char[] arr2 = str2.toCharArray();
+		int count = 0;
+		for(int i = 0; i < arr1.length; i++) {
+			if(arr1[i] != arr2[i]) count++;
+		}
+		return count == 1;
+	}
+}
